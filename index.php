e72c629c580d5c9fbb2843f18e56ac15b5c87629
abdul-local <abdullah.hamzan@gmail.com>
2020-08-12 18:33:53 +0800

    Hello world dan Mix HTML

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// ini komentar baris 1
+# ini komentar baris 2
+/*ini komentar yang banyak */
+//echo "hellow, world";
+
+// $namalengkap=1000;
+// echo $namalengkap;
+// $nama="MALIK";
+// echo  $nama;
+$x=12;
+$y=13;
+$z=$x+$y;
+echo $z;
+
+?>
