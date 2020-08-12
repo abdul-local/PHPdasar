75d8147a6276683097fd74ea7f31edfe6aff066b
abdul-local <abdullah.hamzan@gmail.com>
2020-08-12 20:16:42 +0800

    mengenal fungsi sudah selesai

--- index.php
+++ index.php
@@ -1,34 +1,33 @@
-
 <?php
-    $nama ="M ABDULLOH HAMZAN";
-    $alamat="Lombok";
-    $gol_darah="B";
-    $name ="<h2>Siswa Codepolitan</h2>";
+$name=strtoupper("malik middin");
+$umur=16;
+function pesan(){
+    echo " Selamat Datang Siwa Codepolitan";
+}
+pesan();
 
 ?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Belajar PHP dasar</title>
 </head>
 <body>
-<h1>Selamat Datang, <?php echo $nama ?></h1>
-<h2>Alamat: <?php echo $alamat ?></h2>
-<h2>Golongan Darah: <?php echo $gol_darah ?></h2>
-
-<p>ini adalah paragraf pertama saya di dokumen HTML yang berisi kode PHP.</p>
-<?php
-echo $nama;
-echo "<br>";
-echo $alamat;
-echo "<br>";
-echo $gol_darah;
-echo "<br>";
+<h2>
+SELAMAT DATANG,
+<?php 
 echo $name;
 echo "<br>";
-echo $alamat;
+echo $umur;
 ?>
+
+</h2>
+    
 </body>
 </html>
