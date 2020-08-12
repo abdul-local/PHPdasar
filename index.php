ed50b7364be2cf3fbb74dcf3a877f31144752455
abdul-local <abdullah.hamzan@gmail.com>
2020-08-12 22:45:09 +0800

    materi string php dasar sudah selesai

--- index.php
+++ index.php
@@ -1,45 +1,22 @@
 <?php
-// jumlah
-// $a=10;
-// $b=20;
-// $c=$a+$b;
-// echo $c;
-// pembagian
-// $a=10;
-// $b=3;
-// $c=$a/$b;
-// echo $c;
-//perkalian
-// $a=10;
-// $b=3;
-// $c=$a*$b;
-// echo $c;
-//sisa pembagian
-// $a=10;
-// $b=3;
-// $c=$a%$b;
-// echo $c;
-//menggukan fungsi round untuk membulatakan bilangan
-// $a=10;
-// $b=3;
-// $c=$a/$b;
-// // echo round($c);
-// echo round($c,2);
-//menggukan increment
-// $a=10;
-// $a++;   
-// echo $a;
-// $a=10;
-// $a=$a+1;
-// echo $a;
-//menggunakan variabel dekrement
-// $a=10;
-// $a--;   
-// echo $a;
-$a=10;
-$a=$a-1;
-echo $a;
+// string pada php dasar
+$name="Abdul";
+$pesan="Selamat Datang, ";
+$nama1="Abdul";
+$nama2="Hamzan";
+$nama_lengkap= $nama1.$nama2;
+$pesan1="hallow"." "."assalamu'alaikum";
+echo $nama_lengkap;
+echo " <br> ";
 
+echo $pesan1;
+
+
+//echo ("$pesan  $name");
+// menggunakan dengan cara lain
+// echo $pesan;
+//menggabungkan dengan menggukan titik
+// echo $pesan . $name;
 
 
 
