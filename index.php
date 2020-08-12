b30c5f140c9c34632f831f27a24114bbc1bc7ac1
abdul-local <abdullah.hamzan@gmail.com>
2020-08-12 19:19:58 +0800

    mix HTML dengan PHP

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
-<?php
-// ini komentar baris 1
-# ini komentar baris 2
-/*ini komentar yang banyak */
-//echo "hellow, world";
 
-// $namalengkap=1000;
-// echo $namalengkap;
-// $nama="MALIK";
-// echo  $nama;
-$x=12;
-$y=13;
-$z=$x+$y;
-echo $z;
+<?php
+    $nama ="M ABDULLOH HAMZAN";
+    $alamat="Lombok";
+    $gol_darah="B";
+    $name ="<h2>Siswa Codepolitan</h2>";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>Selamat Datang, <?php echo $nama ?></h1>
+<h2>Alamat: <?php echo $alamat ?></h2>
+<h2>Golongan Darah: <?php echo $gol_darah ?></h2>
+
+<p>ini adalah paragraf pertama saya di dokumen HTML yang berisi kode PHP.</p>
+<?php
+echo $nama;
+echo "<br>";
+echo $alamat;
+echo "<br>";
+echo $gol_darah;
+echo "<br>";
+echo $name;
+echo "<br>";
+echo $alamat;
+?>
+</body>
+</html>
