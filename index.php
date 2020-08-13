52facc3e84bcd1078665208fa475834ffc96af7f
abdul-local <abdullah.hamzan@gmail.com>
2020-08-13 12:52:49 +0800

    Array Multidimensi

--- index.php
+++ index.php
@@ -1,50 +1,48 @@
 <?php
-// Array Asosiatif php dasar
-//cara pertama penulisan array
-// $a=["sapi","kerbau","kambing","domba"];
-//cara kedua penulisan array
-// $a[0]="sapi";
-// $a[1]="kambing";
-// $a[2]="domba";
+// Array Multidimensi php dasar
+// $a = [250,true,["Abdul","Bandung","Golongan darah"]];
 // var_dump($a);
 // echo "<br>";
-//dengan cara 1
-// $datadiri[] = "Abdul Hamzan";
-// $datadiri[] = "B";
-// $datadiri[] = "Bandung";
+// echo $a[2][0];
+
+// $herbivora = ["kambing","sapi","kerbau"];
+// $karnivora = ["harimau","singa","serigala"];
+// $omnivora = ["ayam","monyet","babi"];
+
+//$binatang = [$herbivora, $karnivora, $omnivora];
+// membuat sebuah key
+// $binatang=["herbivora"=>$herbivora,"karnivora"=>$karnivora,"omnivora"=>$omnivora];
+// echo $binatang ["omnivora"][1];
 // echo "<br>";
-// echo "Nama : ".$datadiri[0];
+//menampilkan tulisan sapi
+// echo $binatang ["herbivora"][1];
+
+//memberikan key dan nama
+
+// echo $binatang[2][1];
 // echo "<br>";
-// echo "Golongan darah : ".$datadiri[1];
-// echo "<br>";
-// echo "Alamat:".$datadiri[2];
-//dengan cara 2 yang du sebut dengan asosiatif array
-// $datadiri["nama"] ="Abdul Hamzan";
-// $datadiri["gol"] ="B";
-// $datadiri["Alamat"]="Bandung";
-// ==> khusus untuk mengarah ke nilai yang di inginkan
+// menampilkan sapi
+// echo $binatang[0][1];
 
-$datadiri = [
+$mahasiswa=array(
+    array("Marimar",20,"Wanita"),
+    array("Soledad",25,"Wanita"),
+    array("Alfonso",30,"Pria")
+    
+); 
 
-    "Nama"=>"Muhammad Abdulloh Hamzan",
-    "Golongan darah"=>"B",
-    "Alamat"=>"Bandung"
-];
+// echo $mahasiswa[0][0]. " Memiliki Usia "  .$mahasiswa[0][1]. " jenis kelamin " .$mahasiswa[0][2]."<br>";
+// echo $mahasiswa[1][0]. " Memiliki Usia "  .$mahasiswa[1][1]. " jenis kelamin " .$mahasiswa[1][2]."<br>";
+// echo $mahasiswa[2][0]. " Memiliki Usia "  .$mahasiswa[2][1]. " jenis kelamin " .$mahasiswa[2][2];
+for($baris=0; $baris<3; $baris++){
+    echo "<p><b> Baris No: $baris</b></p>";
+    echo "<ul>";
+    for($kolom=0; $kolom<3; $kolom++){
+        echo "<li>".$mahasiswa[$baris][$kolom]."</li>";
 
-// echo "Nama:".$datadiri['nama'];
-// echo "<br>";
-// echo "Alamat:".$datadiri['Alamat'];
-// echo "<br>";
-// echo "Golongan darah:".$datadiri['gol'];
+    }
+    echo "</ul>";
+}
 
-echo $datadiri; 
-
-
-
-// var_dump($datadiri);
-
-
-
-// echo $a[0];
 
 ?>
