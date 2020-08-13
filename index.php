929e63891bdfd937a91198568f43429baa84adae
abdul-local <abdullah.hamzan@gmail.com>
2020-08-13 19:15:45 +0800

    membuat form PHP dasar sudah selesai

--- index.php
+++ index.php
@@ -1,21 +1,82 @@
-<?php
-// Tipe data Null
-// null merepresentasikan tidak ada tipe data atau kosong
-$pesan=null;
-$datadiri = [
-    'nama' => 'abdul',
-    "alamat" => "Lombok",
-    "hobi" => "programing"
-];
-// mencetak variabel data diri dengan index nama
+<!-- Method post-->
+<!-- 
+<form action="" method="post">
+Nama: <input type="text" name="nama">
+Alamat: <input type="text" name="alamat"><br><br>
+<input type="submit" value="submit">
+</form> -->
 
-//echo $datadiri['nama']."<br>";
-//mencba panggil dengan index goldarah yg tidak ada di array data diri
-//echo $datadiri['goldarah'];
-// echo $pesan;
-//mencoba menampilkan data yag tidak ada nilainya 
-echo $datadiri['malik'] ?? null;
+<!-- method get -->
+<!-- <form action="proses.php" method="get"> 
+Nama: <input type="text" name="nama">
+Alamat: <input type="text" name="alamat"><br><br>
+<input type="submit" value="submit">
+</form>  -->
+
+<!-- jenis yang sering di gunakan utk menginput data string -->
+<form action="" method="post">
+Nama: <input type="text" name="nama">
+<input type="submit" value="submit">
+<br>
+<br>
+<br>
+</form>
+<!-- inputan dengan text area -->
+<form action="" method="post">
+Alamat: <textarea name="alamat"  col="200" rows="8"></textarea>
+<br><br>
+<input type="submit" value="submit">
+<br><br>
+</form>
+<!-- inputan dengan password -->
+<form action="" method="post">
+Password: <input type="password" name="password">
+<input type="submit" value="submit">
+</form>
+<!-- inputan dengan tipe date -->
+<br>
+<br>
+<form action="" method="post">
+Tanggal beli: <input type="date" name="tanggal">
+<input type="submit" value="submit">
+</form>
+<br>
+<br>
+<form action="" method="post">
+<input type="button" value="ini Button" onClick="alert('hello)">
+</form>
+<br>
+<br>
+<!-- inputan dengan tipe data checkbox -->
+<form action="" method="post">
+<input type="checkbox" name="php" value="php">PHP <br>
+<input type="checkbox" name="HTML" value="HTML">HTML <br>
+<input type="checkbox" name="Javascript" value="javasript">Javascript
+</form>
+<br>
+<br>
+<!-- inputan dengan option -->
+<form action="" method="post">
+<select name="mahasiswa">
+<option value="TI">Teknik Imformatika</option>
+<option value="TE">Teknik Elektro</option>
+<option value="TM">Teknik Mesin</option>
+</select>
+</form>
+<br>
+<br>
+<!-- inputan dengan radio -->
+<form action="" method="post">
+<input type="radio" name="program" value="TI"> Sistem Informatika <br>
+<input type="radio" name="program" value="TE"> Teknik Mesin <br>
+<input type="radio" name="program" vale="MT"> Magister Teknik
+</form>
+<br>
+<br>
+<!-- inputan dengan file --> 
+<form action="" method="post">
+<input type="file" value="document">
+</form>
 
 
 
-?>
