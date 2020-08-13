d7fda4dd8d406d878f8966f458dbe9315f80a3b3
abdul-local <abdullah.hamzan@gmail.com>
2020-08-14 02:00:15 +0800

    if Bersarang php dasar

--- index.php
+++ index.php
@@ -1,13 +1,37 @@
 <?php
-$nilai=40;
-if($nilai>=80){
-    echo " Selamat Anda Mendapatkan A";
-}else if($nilai >=70){
-    echo " Selamat Anda Mendapatkan B";
-}else if($nilai >=50){
-    echo " Selamat Anda Mendapatkan C";
+// $nilai_tes = 85;
+// $wawancara = 'A';
+
+// bila test lebih dari 70
+// if ($nilai_tes >= 70){
+//     echo "Anda lulus";
+//     if($wawancara =='A'){
+//         echo "Predikat Anda sangat memuakan";
+//     }else if($wawancara =='B'){
+//         echo "predikat Anda memuaskan";
+//     }else{
+//         echo "predikat Anda cukup";
+//     }
+// }else{
+//     echo " Maaf Anda belum lulus ";
+// }
+$psikotes=85;
+$wawancara='lulus';
+$kesehatan='lulus';
+if($psikotes>=80){
+    echo "Anda lolo psikotest ";
+    if($wawancara ='lulus'){
+        echo "Selamat anda lolos di test Wawancara ";
+        if($kesehatan ='lulus'){
+            echo "Selamat Anda di terima jadi pegawai BUMN";
+        }else{
+            echo "Mohon maaf anda di pulangkan";
+        }
+    }else{
+        echo "mohon maaf anda tidak lolos";
+    }
 }else{
-    echo "Selamat Anda mendapatkan D+";
+    echo "Mohon maaf anda tidak lolos";
 }
 
 ?>
