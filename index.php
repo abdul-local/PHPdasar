3147f533f67ad9bd8a6d0e868771ff6407ccaa37
abdul-local <abdullah.hamzan@gmail.com>
2020-08-14 01:21:27 +0800

    if elseif else php dasar

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <?php
-// materi if else mengacu ke kondisi
-// $nama="abdul";
-
-// ondisi jika di variabel nama itu sama dengan string abdul maka akan menampilkan tulisan
-// if($nama="abdul"){
-//     echo " selamat datang , abdul di codepolitan";
-// menggunakan sample yang lain
-$nilai =80;
-
-if($nilai>= 50){
-    echo "Anda Lulus Ujian dengan Nilai ".$nilai;
+$nilai=40;
+if($nilai>=80){
+    echo " Selamat Anda Mendapatkan A";
+}else if($nilai >=70){
+    echo " Selamat Anda Mendapatkan B";
+}else if($nilai >=50){
+    echo " Selamat Anda Mendapatkan C";
 }else{
-    echo " Maaf Anda belum Lulus Ujian dengan Nilai ".$nilai;
+    echo "Selamat Anda mendapatkan D+";
 }
 
-// dengan mengguakan ini maka lebih rumit
-// if($nilai<50){
-//     echo " Maaf anda belum lulus ujian dengan Nilai ".$nilai;
-// }
-
-// ?>
+?>
