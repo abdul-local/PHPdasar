8278f0910c93b7ae57af7dd9d0ee76a3707b98d4
abdul-local <abdullah.hamzan@gmail.com>
2020-08-13 14:23:00 +0800

    tipe data null sudah selesai

--- index.php
+++ index.php
@@ -1,48 +1,21 @@
 <?php
-// Array Multidimensi php dasar
-// $a = [250,true,["Abdul","Bandung","Golongan darah"]];
-// var_dump($a);
-// echo "<br>";
-// echo $a[2][0];
+// Tipe data Null
+// null merepresentasikan tidak ada tipe data atau kosong
+$pesan=null;
+$datadiri = [
+    'nama' => 'abdul',
+    "alamat" => "Lombok",
+    "hobi" => "programing"
+];
+// mencetak variabel data diri dengan index nama
 
-// $herbivora = ["kambing","sapi","kerbau"];
-// $karnivora = ["harimau","singa","serigala"];
-// $omnivora = ["ayam","monyet","babi"];
+//echo $datadiri['nama']."<br>";
+//mencba panggil dengan index goldarah yg tidak ada di array data diri
+//echo $datadiri['goldarah'];
+// echo $pesan;
+//mencoba menampilkan data yag tidak ada nilainya 
+echo $datadiri['malik'] ?? null;
 
-//$binatang = [$herbivora, $karnivora, $omnivora];
-// membuat sebuah key
-// $binatang=["herbivora"=>$herbivora,"karnivora"=>$karnivora,"omnivora"=>$omnivora];
-// echo $binatang ["omnivora"][1];
-// echo "<br>";
-//menampilkan tulisan sapi
-// echo $binatang ["herbivora"][1];
-
-//memberikan key dan nama
-
-// echo $binatang[2][1];
-// echo "<br>";
-// menampilkan sapi
-// echo $binatang[0][1];
-
-$mahasiswa=array(
-    array("Marimar",20,"Wanita"),
-    array("Soledad",25,"Wanita"),
-    array("Alfonso",30,"Pria")
-    
-); 
-
-// echo $mahasiswa[0][0]. " Memiliki Usia "  .$mahasiswa[0][1]. " jenis kelamin " .$mahasiswa[0][2]."<br>";
-// echo $mahasiswa[1][0]. " Memiliki Usia "  .$mahasiswa[1][1]. " jenis kelamin " .$mahasiswa[1][2]."<br>";
-// echo $mahasiswa[2][0]. " Memiliki Usia "  .$mahasiswa[2][1]. " jenis kelamin " .$mahasiswa[2][2];
-for($baris=0; $baris<3; $baris++){
-    echo "<p><b> Baris No: $baris</b></p>";
-    echo "<ul>";
-    for($kolom=0; $kolom<3; $kolom++){
-        echo "<li>".$mahasiswa[$baris][$kolom]."</li>";
-
-    }
-    echo "</ul>";
-}
 
 
 ?>
