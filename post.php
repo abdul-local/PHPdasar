83122bf6a0b85230727baab733b381465e68b4fd
abdul-local <abdullah.hamzan@gmail.com>
2020-08-13 23:01:55 +0800

    Global Variabel post sudah selesai

---
+++ post.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
+</head>
+<body>
+    <form action="proses2.php" method="post">
+    Nama: <input type="text" name="nama">
+    Alamat: <input type="text" name="alamat"><br><br>
+    <input type="submit" value="submit">
+    </form>
+</body>
+</html>
---
+++ proses2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>proes2</title>
+</head>
+<body>
+   <h1>Selama datang, <?php echo $_POST['nama']; ?> di codepolitan</h1>
+   <h2> Saya Tinggal di <?php echo $_POST['alamat'];?></h2>
+</body>
+</html>
