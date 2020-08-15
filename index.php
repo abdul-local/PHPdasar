cf30cb8f0b7fafb5830039bc441f0f79fd7952db
abdul-local <abdullah.hamzan@gmail.com>
2020-08-15 10:46:41 +0800

    do while php dasar sudah selesai

--- index.php
+++ index.php
@@ -1,32 +1,9 @@
 <?php
-$warna = ['merah','biru','kuning','merah','hijau','orange','ungu','biru','kuning','merah','hijau','orange','ungu','merah'];
-
-$jumlah_merah = 0;
-$jumlah_biru=0;
-// kita mencari jumlah merah di dalam array dengan secara manual dulu
-// if($warna[0] ='merah'){
-//     $jumlah_merah ++;
-
-// }
-// if($warna[1]="merah"){
-//     $jumlah_merah ++;
-// }
-// if($warna[2]="merah"){
-//     $jumlah_merah ++;
-// }
-// echo $jumlah_merah;
-// menggunakan cara ke yang lebih simple
-// for($i=0; $i<14; $i++){
-//     if($warna[$i] == 'merah'){
-//         $jumlah_merah=$jumlah_merah+1;
-//     }
-   
-// }
-//pengecekan jumlah warna biru
-for($i=0; $i<14; $i++){
-    if($warna[$i] == 'biru'){
-        $jumlah_biru=$jumlah_biru+1;
-    }
+//menggunakan dengan do while
+$x=1;
+do {
+    echo "nomer antrian dari : " .$x."<br>";
+    $x++;
 }
-echo "Jumlah warna biru adalah : " .$jumlah_biru;
+while($x<15);
 ?>
