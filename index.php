421ef2417fbce7eaa75d7576f57d2810bd3a8ab4
abdul-local <abdullah.hamzan@gmail.com>
2020-08-15 17:57:46 +0800

    Membuat fungsi sendiri

--- index.php
+++ index.php
@@ -1,39 +1,21 @@
 <?php
-// continue php dasar
-$siswa = [
-    ['nama'=> 'toni','kelas'=>'2A','goldarah'=>'B'],
-    ['nama'=> 'kresna','kelas'=>'2A','goldarah'=>'AB'],
-    ['nama'=> 'Palupi','kelas'=>'2B','goldarah'=>'B'],
-    ['nama'=> 'Singgih','kelas'=>'2C','goldarah'=>'A'],
-    ['nama'=> 'Oriza','kelas'=>'2B','goldarah'=>'O'],
-    ['nama'=> 'hani','kelas'=>'2D','goldarah'=>'B']
-];
+    // $kalimat = " saya suka pizza, terutama pizza yang dagingnya tebal ";
+    //menggunakan fungsi str_word_count
+    // str_word_count berfungsi untuk menghitung jumlah kalimat
+    // echo str_word_count($kalimat);
+    //fungsi str_replace berfungsi untuk mengganti kata 
+    // echo str_replace('pizza','burger',$kalimat);
+
+    //membuat fungsi sendiri 
+    function cetakpesan($nama = null){
+        if(empty($nama)){
+            echo "Selamat datang di codepolitan ";
+        }else{
+            echo "Selamat datang di codepolitan, $nama";
+        }
+        
+    }
+    cetakpesan();
+    
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menampilkan data ke HTML</title>
-</head>
-<body>
-    <table border="1">
-    <tr>
-    <th>Nama</th>
-    <th>kelas</th>
-    <th>goldarah</th>
-    </tr>
-    <?php
-    foreach($siswa as $key=>$nilai): ?>
-        <tr>
-        <td><?php echo $nilai['nama']; ?></td>
-        <td><?php echo $nilai['kelas']; ?></td>
-        <td><?php echo $nilai['goldarah']; ?></td>
-        </tr>
-
-    <?php endforeach;?>
-    </table>
-</body>
-</html>
