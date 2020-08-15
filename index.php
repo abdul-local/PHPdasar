d093ddd09172837d084d89649f273e70d6fcfd68
abdul-local <abdullah.hamzan@gmail.com>
2020-08-15 12:25:03 +0800

    continue php dasar sudah selesai

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
-// menggunakan perintah foreach dan break
-$nama = ['budi','hipzil','mawar','zaskia','rodi','abdul','huswah','rendi','fakih','nuro','muzhir','dian','agus','zoh'];
-foreach($nama as $key => $nilai){
-    echo"Nama:$nilai"."<br>";
-    if($nilai == 'abdul'){
-        echo " Abdul ada di indek ke $key "."<br>";
-        // kita ingin menghentikan apabila sudah ketemu nama abdul maka kita menggunakan break
-    break;
+// continue php dasar
+$siswa = [
+    ['nama'=> 'toni','kelas'=>'2A'],
+    ['nama'=> 'kresna','kelas'=>'2A'],
+    ['nama'=> 'Palupi','kelas'=>'2B'],
+    ['nama'=> 'Singgih','kelas'=>'2C'],
+    ['nama'=> 'Oriza','kelas'=>'2B'],
+    ['nama'=> 'hani','kelas'=>'2D']
+];
+foreach($siswa as $key => $nilai){
+    //cara pertama
+    // if($nilai['kelas'] !== '2A')
+    // {
+    // echo "Nama : ".$nilai['nama']."<br>";
+    // echo "Kelas :" .$nilai['kelas']."<br>";
+
+    // }
+
+    // dengan menggunakan perintah continue untuk melanjutkan ekskusi setelahnya.
+    if($nilai['kelas'] =='2A'){
+        continue;
     }
+    echo "Nama: ".$nilai['nama']."<br>";
+    echo "Kelas: " .$nilai['kelas']."<br>";
+
+
+    
 }
 
 ?>
