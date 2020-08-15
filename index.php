f0214b1e3119dcb46ddb3da393aa0ce02603bc61
abdul-local <abdullah.hamzan@gmail.com>
2020-08-15 20:20:08 +0800

    mengembalikan nilai fungsi php dasar

--- index.php
+++ index.php
@@ -1,39 +1,22 @@
 <?php
-    // $kalimat = " saya suka pizza, terutama pizza yang dagingnya tebal ";
-    //menggunakan fungsi str_word_count
-    // str_word_count berfungsi untuk menghitung jumlah kalimat
-    // echo str_word_count($kalimat);
-    //fungsi str_replace berfungsi untuk mengganti kata 
-    // echo str_replace('pizza','burger',$kalimat);
 
-    //membuat fungsi sendiri 
-    // function cetakpesan($nama = null){
-    //     global $nama;
-    //     if(empty($nama)){
-    //         echo "Selamat datang di codepolitan ";
-    //     }else{
-    //         echo "Selamat datang di codepolitan, $nama";
-    //     }
-        
-    // }
-    // $nama="Abdul Hamzan";
-    // cetakpesan($nama);
-
-    // $nama='abdul';
-    // function cetakpesan($nama){
-    //     echo " Nama $nama , selamat datang di codepolitan";
-    // }
-    // cetakpesan($nama);
-     // bisa menggunakan dengan mendeklarasikan variael global di dalam fungsi
-
-     // variabel global dan isinya
-     $nama ="malik";
-     function tulispesan(){
-         global $nama;
-         echo "Selamat datamg $nama di codepolitan";
-     }
-     tulispesan();
-
-    
+$alas=10;
+$tinggi=25;
+// $kalimat = "Saya suka pizza,yang dagingnnya tebal ";
+// echo str_replace('pizza','hamburger',$kalimat);
+// echo $kalimat;
+function luassegitaga($alas,$tinggi)
+{
+    $luas =($alas * $tinggi)/2 ;
+    return $luas;
+}
+echo luassegitaga($alas,$tinggi)."<br>";
+    // $hasil = luassegitaga($alas,$tinggi);
+    // echo $hasil;
+function luasjajargenjang($alas,$tinggi){
+    $luas = $alas * $tinggi ;
+    return $luas;
+}
+echo luasjajargenjang($alas,$tinggi);
 
 ?>
