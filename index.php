19f7b6764ef6db6aa249c48bbcbc473ef7a82c95
abdul-local <abdullah.hamzan@gmail.com>
2020-08-15 13:45:37 +0800

    menampilkan data ke tabel sudah selesai

--- index.php
+++ index.php
@@ -1,31 +1,39 @@
 <?php
 // continue php dasar
 $siswa = [
-    ['nama'=> 'toni','kelas'=>'2A'],
-    ['nama'=> 'kresna','kelas'=>'2A'],
-    ['nama'=> 'Palupi','kelas'=>'2B'],
-    ['nama'=> 'Singgih','kelas'=>'2C'],
-    ['nama'=> 'Oriza','kelas'=>'2B'],
-    ['nama'=> 'hani','kelas'=>'2D']
+    ['nama'=> 'toni','kelas'=>'2A','goldarah'=>'B'],
+    ['nama'=> 'kresna','kelas'=>'2A','goldarah'=>'AB'],
+    ['nama'=> 'Palupi','kelas'=>'2B','goldarah'=>'B'],
+    ['nama'=> 'Singgih','kelas'=>'2C','goldarah'=>'A'],
+    ['nama'=> 'Oriza','kelas'=>'2B','goldarah'=>'O'],
+    ['nama'=> 'hani','kelas'=>'2D','goldarah'=>'B']
 ];
-foreach($siswa as $key => $nilai){
-    //cara pertama
-    // if($nilai['kelas'] !== '2A')
-    // {
-    // echo "Nama : ".$nilai['nama']."<br>";
-    // echo "Kelas :" .$nilai['kelas']."<br>";
-
-    // }
-
-    // dengan menggunakan perintah continue untuk melanjutkan ekskusi setelahnya.
-    if($nilai['kelas'] =='2A'){
-        continue;
-    }
-    echo "Nama: ".$nilai['nama']."<br>";
-    echo "Kelas: " .$nilai['kelas']."<br>";
-
-
-    
-}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menampilkan data ke HTML</title>
+</head>
+<body>
+    <table border="1">
+    <tr>
+    <th>Nama</th>
+    <th>kelas</th>
+    <th>goldarah</th>
+    </tr>
+    <?php
+    foreach($siswa as $key=>$nilai): ?>
+        <tr>
+        <td><?php echo $nilai['nama']; ?></td>
+        <td><?php echo $nilai['kelas']; ?></td>
+        <td><?php echo $nilai['goldarah']; ?></td>
+        </tr>
+
+    <?php endforeach;?>
+    </table>
+</body>
+</html>
