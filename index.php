2d05f4f5d5ec03e27a2e8f9b79883c7306f35cf5
abdul-local <abdullah.hamzan@gmail.com>
2020-08-15 11:41:36 +0800

    break pada perulangan sudah selesai

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-// menggunakan perintah foreach
-$warna = ['merah','biru','kuning','merah','hijau','orange','ungu','biru','kuning','merah','hijau','orange','ungu','merah'];
-
-$jumlah_merah = 0;
-foreach( $warna as $key =>$value){
-    echo " index ke $key berisi $value"."<br>";
-    if($value=='merah'){
-        $jumlah_merah=$jumlah_merah+1;
+// menggunakan perintah foreach dan break
+$nama = ['budi','hipzil','mawar','zaskia','rodi','abdul','huswah','rendi','fakih','nuro','muzhir','dian','agus','zoh'];
+foreach($nama as $key => $nilai){
+    echo"Nama:$nilai"."<br>";
+    if($nilai == 'abdul'){
+        echo " Abdul ada di indek ke $key "."<br>";
+        // kita ingin menghentikan apabila sudah ketemu nama abdul maka kita menggunakan break
+    break;
     }
 }
-echo" Jumlah warna merah adalah: ".$jumlah_merah;
+
 ?>
