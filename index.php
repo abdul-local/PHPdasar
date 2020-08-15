e64cd39b71990dfdc2049e976b6f48cefdfb7ae3
abdul-local <abdullah.hamzan@gmail.com>
2020-08-15 18:22:43 +0800

    Skup Variabel php dasar

--- index.php
+++ index.php
@@ -7,15 +7,33 @@
     // echo str_replace('pizza','burger',$kalimat);
 
     //membuat fungsi sendiri 
-    function cetakpesan($nama = null){
-        if(empty($nama)){
-            echo "Selamat datang di codepolitan ";
-        }else{
-            echo "Selamat datang di codepolitan, $nama";
-        }
+    // function cetakpesan($nama = null){
+    //     global $nama;
+    //     if(empty($nama)){
+    //         echo "Selamat datang di codepolitan ";
+    //     }else{
+    //         echo "Selamat datang di codepolitan, $nama";
+    //     }
         
-    }
-    cetakpesan();
+    // }
+    // $nama="Abdul Hamzan";
+    // cetakpesan($nama);
+
+    // $nama='abdul';
+    // function cetakpesan($nama){
+    //     echo " Nama $nama , selamat datang di codepolitan";
+    // }
+    // cetakpesan($nama);
+     // bisa menggunakan dengan mendeklarasikan variael global di dalam fungsi
+
+     // variabel global dan isinya
+     $nama ="malik";
+     function tulispesan(){
+         global $nama;
+         echo "Selamat datamg $nama di codepolitan";
+     }
+     tulispesan();
+
     
 
 ?>
