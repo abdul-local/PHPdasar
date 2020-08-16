a7fb4caaad1b7df560aa873dced4c8fdf97ebb86
abdul-local <abdullah.hamzan@gmail.com>
2020-08-16 08:24:36 +0800

    menulis dan membaca file

--- index.php
+++ index.php
@@ -1,23 +1,9 @@
-<?php 
-// function gantiNama(&$nama){
-//     $nama = "Nama baru";
-// }
-// $nama="Toni";
-// gantiNama($nama);
-// echo $nama;
-function kalilipat(&$angka){
-    $angka =$angka * 2;
-}
-$x = 5;
-echo $x ."<br>";
-kalilipat($x);
-echo $x .'<br>';
-kalilipat($x);
-echo $x .'<br>';
-kalilipat($x);
-echo $x .'<br>';
-kalilipat($x);
-echo $x .'<br>';
-kalilipat($x);
-echo $x .'<br>';
+<?php
+$message="hellow nama saya muhammad abdulloh hamza, saya seorang member regular di codepolitan ";
+// fungsi file_put_contents untuk meulis data dalam bentuk file txt
+file_put_contents("content.txt", $message);
+// fungsi file_get_contents untuk membaca atay menampilkan hasil tulisan yang sebelumnya ke dalam file php
+$pesan=file_get_contents("content.txt");
+echo $pesan;
+
 ?>
