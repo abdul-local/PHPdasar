76d89835c4dbcb09ff990442472b5612a0d81b64
abdul-local <abdullah.hamzan@gmail.com>
2020-08-16 20:52:48 +0800

    membuat app todo menyimpan data ke file udah selesai

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
+<?php
+// kita mengguanakan fungsi isset() untuk memeriksa suatu object dari inputan form
+$todos=[];
+// saya mau baca file yang sy tulis di post itu
+if(file_exists('todo.txt')){
+    $file = file_get_contents('todo.txt');
+    $todos= unserialize($file);
+}
+
+
+
+if(isset($_POST['todo'])){
+    $data=$_POST['todo'];
+    $todos[]=[
+        'todo'=>$data,
+        'status'=> 0
+    ];
+    file_put_contents('todo.txt',serialize($todos));
+}
+
+?>
+
+
 <h2>Todo App</h2>
-<form action="">
+<form action="" method="POST">
 <label for="">Daptar Belanja Hari ini</label><br>
 <input type="text" name='todo'>
 <button type"submit"> simpan</button>
