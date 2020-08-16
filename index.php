9622e4503cf34113bf33cb21add766dedcb85f13
abdul-local <abdullah.hamzan@gmail.com>
2020-08-16 10:28:34 +0800

    Membuat Aplikasi Todo Membuat Template

--- index.php
+++ index.php
@@ -1,42 +1,20 @@
-<?php
-$siswa=[
-    ['nama'=>'abdul','goldarah'=>'B'],
-    ['nama'=>'hamzan','goldarah'=>'AB'],
-    ['nama'=>'rodi','goldarah'=>'A'],
-    ['nama'=>'huswah','goldarah'=>'B+']
-];
-// menggunakan fungsi serialize untuk mengubah data ke dalam bentuk string
-// $data=serialize($siswa);
-// cetak data
-// echo $data;
-// tuli file tesb kedalam file yang sudah di berikan
-//  file_put_contents("data.txt",$data);
- // saya baca file yang ada di data.txt
-//  $output= file_get_contents("data.txt");
- // cetak output
-//  echo $output;
-//kemudian saya mau ubah data yang berbentuk serialize ke dalam bentuk array degan fungsi unserialize
-// $hasil=unserialize($output);
-//cetak hasil berbentuk array menggunakan fungsi print_r
-// print_r($hasil);
-
-// mengubah data ke dalam bentuk json
-// kita menggunakan printah json_encode
-$data= json_encode($siswa);
-// // cetak data
-// echo $data;
-//tulis file ke dalam data.txt
-file_put_contents("data.txt",$data);
-// baca file yang di tulis dan tampilkan
-$output = file_get_contents("data.txt");
-//cetak output
-// echo $output;
-// ubah filenya kedalam bentuk array dengan fugsi json_decode
-$hasil= json_decode($output,true);
-// cetak hasil
-print_r($hasil);
-
-
-
-
-?>
+<h2>Todo App</h2>
+<form action="">
+<label for="">Daptar Belanja Hari ini</label><br>
+<input type="text" name='todo'>
+<button type"submit"> simpan</button>
+</form>
+<ul>
+<li>
+<input type="checkbox" name="todo1">Todo 1
+<a href="#">hapus</a>
+</li>
+<li>
+<input type="checkbox" name="todo2">Todo 2
+<a href="#">hapus</a>
+</li>
+<li>
+<input type="checkbox" name="todo3">Todo 3
+<a href="#">hapus</a>
+</li>
+</ul>
