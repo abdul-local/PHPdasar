932ffd48016a0f4717203165c4f2df9898f3b067
abdul-local <abdullah.hamzan@gmail.com>
2020-08-18 18:54:27 +0800

    memperbaharui status pada app todo sudah selesai

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
-// kita mengguanakan fungsi isset() untuk memeriksa suatu object dari inputan form
+
 $todos=[];
-// saya mau baca file yang sy tulis di post itu
+// funtion file_exist berfungsi untuk mengcek file apakah sudah ada atau belum
 if(file_exists('todo.txt')){
+    // saya mau baca file yang sy tulis di post itu
     $file = file_get_contents('todo.txt');
     $todos= unserialize($file);
 }
 
 
-
+// kita mengguanakan fungsi isset() untuk memeriksa suatu object dari inputan form
 if(isset($_POST['todo'])){
     $data=$_POST['todo'];
     $todos[]=[
@@ -16,7 +17,17 @@ if(isset($_POST['todo'])){
         'status'=> 0
     ];
     file_put_contents('todo.txt',serialize($todos));
+    // redirect ke halaman denga fugsi header
+    header('location:index.php');
 }
+if(isset($_GET['status'])){
+    $todos[$_GET['key']]['status'] = $_GET['status'];
+    file_put_contents('todo.txt',serialize($todos));
+    //sy redirect dengan fungsi heder biar ndk berulang
+    header('location:index.php');
+}
+
+print_r($todos);
 
 ?>
 
@@ -31,8 +42,16 @@ if(isset($_POST['todo'])){
 <?php
 foreach($todos as $key=>$nilai): ?>
 <li>
-<input type="checkbox" name="todo">
-<label for=""><?php echo $nilai['todo'] ?></label>
+<input type="checkbox" name="todo" onclick="window.location.href='index.php?status=<?php if($nilai['status']== 1) echo 0; else echo 1; ?>&key=<?php echo $key; ?>' " <?php if($nilai['status'] == 1) echo 'checked'; ?>> 
+<label ><?php
+    if($nilai['status'] == 1){
+        echo "<del>".$nilai['todo']."</del>";
+    }else
+
+    echo $nilai['todo'];
+
+
+?></label>
 <a href="#">hapus</a>
 </li>
 <?php endforeach; ?>
